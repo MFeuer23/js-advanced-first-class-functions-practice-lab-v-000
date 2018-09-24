ce83083e358070d2c3865c5267f8df5d84af0077
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-24 14:17:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  array.forEach(function cb (el, i, array) {
+    console.log(el.name);
+  })
+}
+
+function logDriversByHometown(array, location) {
+  array.forEach(function cb (el, i, array) {
+    if (el.hometown === location) {
+      console.log(el.name);
+    }
+  })
+}
+
+function driversByRevenue(array) {
+  return array.sort(function cb (a, b) {
+    b.revenue - a.revenue;
+  })
+}
